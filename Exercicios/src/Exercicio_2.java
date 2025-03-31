b58c5b36c6d712757bf8bc0b900f9f35b41db64a
bobby-coder-dev <bobi77713@gmail.com>
2025-03-31 07:49:34 -0300

    comentários

--- Exercicios/src/Exercicio_2.java
+++ Exercicios/src/Exercicio_2.java
@@ -2,14 +2,14 @@ public class Exercicio_2 {
     public static void main(String[] args) {
         int nota1 = Integer.parseInt(args[0]);
         int nota2 = Integer.parseInt(args[1]);
-        int partcipacao = Integer.parseInt(args[2]);
+        int participacao = Integer.parseInt(args[2]);
         int peso1 = 2;
         int peso2 = 3;
 
         double cf1 = ((nota1 * peso1) + (nota2 * peso2))/ (double)(peso1 + peso2) * 0.9;
-        double cf2 = partcipacao * 0.1;
+        double cf2 = participacao * 0.1;
         double cf = cf1 + cf2;
-
+        //calculo da média entre partes
 
         System.out.println(cf);
         if (cf >= 6){
--- Exercicios/src/Exercicio_3.java
+++ Exercicios/src/Exercicio_3.java
@@ -7,11 +7,12 @@ public class Exercicio_3 {
         int quantidade = sc.nextInt();
 
         int[] faixas = new int[4];
+        //vetor para obter o numero de notas presentes em cada faixa sendo armazenadas nos indíces
 
         System.out.println("Entre com a nota dos alunos separadas por espaço: ");
 
         for (int i = 0; i < quantidade; i++) {
-            double nota = sc.nextDouble();
+            double nota = sc.nextDouble(); //leitura contínua dos inteiros
             if (nota >= 0 && nota <= 2){
                 faixas[0]++;
             } else if (nota >= 3 && nota <= 5){
@@ -25,9 +26,9 @@ public class Exercicio_3 {
 
         sc.close();
 
-        System.out.println("0 − 2  | "+"*".repeat(faixas[0]));
-        System.out.println("3 − 5  | "+"*".repeat(faixas[1]));
-        System.out.println("6 − 8  | "+"*".repeat(faixas[2]));
+        System.out.println("0 − 2   | "+"*".repeat(faixas[0]));
+        System.out.println("3 − 5   | "+"*".repeat(faixas[1]));
+        System.out.println("6 − 8   | "+"*".repeat(faixas[2]));
         System.out.println("9 − 10  | "+"*".repeat(faixas[3]));
 
     }
